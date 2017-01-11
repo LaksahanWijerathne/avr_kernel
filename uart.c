6cb2a8adfd3877ec5cb67c021b0621ae1486a95e
weerdmonk <jp.sahoo1991@gmail.com>
2017-01-11 23:06:43 +0530

    Changes to UART module.   External functions will call internal ones only   when debugger is disabled.
      Callbacks to capture character on recv interrupt.
    
      Reg/dereg callbacks.
    
      Enable/disable callbacks.
    
      Peek at a character or string.

--- uart.c
+++ uart.c
@@ -6,6 +6,7 @@
  */
 
 #include "common.h"
+#include "kernel.h"
 
 #define BAUD_RATE_DEFAULT	9600
 
@@ -24,8 +25,26 @@ unsigned char buf_uart_rx[c_UART_RXBUFLEN];
 
 char UART_CLR[] = {27, '[','H', 27, '[', '2', 'J',0};
 char UART_NL[] = {0x0d,0x0a,0};
-	
+
+extern uint8_t v_sys_state;
+
+#define c_MAX_UART_RECV_CB      2	
+
+typedef void(*fp_uart_recv_cb)(void*);
+
+void (*fp_uart_cb_arr[c_MAX_UART_RECV_CB])(void*);
+
+/*
+ * Replace index variables with bitmasks.
+ */
+uint8_t v_recv_cb_reg_idx;
+
+char v_uart_recv_match[c_MAX_UART_RECV_CB];
+
+boolean v_uart_cb_enabled;
+
 void f_uart_flush() {
+	
   p_uart_txin = p_uart_txout = buf_uart_tx;
 	p_uart_rxin = p_uart_rxout = buf_uart_rx;
 
@@ -33,6 +52,15 @@ void f_uart_flush() {
 	v_uart_rxbuf_count = 0;
 
 }
+
+void f_uart_flush_ext() {
+	if (v_sys_state & bm_DBGEN) {
+		return;
+	}		
+	else {
+		f_uart_flush();
+	}
+}	
 
 void f_config_uart(void) {
 	
@@ -45,6 +73,65 @@ void f_config_uart(void) {
 
   f_uart_flush();
 
+  v_uart_cb_enabled = FALSE;
+}
+
+boolean f_reg_uart_cb(fp_uart_recv_cb hook, char c) {
+	
+	if (hook == NULL) {
+    return FALSE;
+  }
+  
+  if (v_recv_cb_reg_idx == c_MAX_UART_RECV_CB) {
+    /* cannot register anymore callbacks */
+    return FALSE;
+  }
+  
+  fp_uart_cb_arr[v_recv_cb_reg_idx] = hook;
+  v_uart_recv_match[v_recv_cb_reg_idx] = c;
+  
+  v_recv_cb_reg_idx++;  
+  
+  return TRUE;
+}
+
+boolean f_dereg_uart_cb(fp_uart_recv_cb hook, char c) {
+  boolean v_is_cb_found = FALSE;
+
+  if (hook == NULL) {
+    return v_is_cb_found;
+  }
+
+  uint8_t i;
+	for (i = 0; i < v_recv_cb_reg_idx; i++) {
+    if (fp_uart_cb_arr[i] == hook) {
+      fp_uart_cb_arr[i] = NULL;
+      v_is_cb_found = TRUE;
+      
+      /*
+       * TODO: accomadate more than 2 callbacks
+       *       OR change this!
+       */
+    }
+    if ( (v_is_cb_found == TRUE) && ((i + 1) < v_recv_cb_reg_idx) ) {
+      fp_uart_cb_arr[i] = fp_uart_cb_arr[i + 1];
+      v_uart_recv_match[i] = v_uart_recv_match[i + 1];
+    }
+  }
+  
+  if (v_is_cb_found == TRUE) {
+    v_recv_cb_reg_idx--;
+  }    
+
+  return v_is_cb_found;
+}
+
+void f_uart_enable_callbacks() {
+  v_uart_cb_enabled = TRUE;
+}  
+
+void f_uart_disable_callbacks() {
+  v_uart_cb_enabled = FALSE;
 }
 
 ISR(USART_UDRE_vect) {
@@ -64,6 +151,21 @@ ISR(USART_UDRE_vect) {
 ISR(USART_RX_vect) {
 	*p_uart_rxin = UDR0;
 
+  uint8_t i;
+  if (v_uart_cb_enabled == TRUE) {
+	  for (i = 0; i < v_recv_cb_reg_idx; i++) {
+		  /*
+		   * if received character matches registered character
+		   * process callback and return
+		   * don't store character in buffer
+		   */
+	    if (v_uart_recv_match[i] == *p_uart_rxin) {
+        (*fp_uart_cb_arr[i])(NULL);
+        return;
+	    } 
+	  }
+  }    
+
 	++v_uart_rxbuf_count;
 
 	if(++p_uart_rxin >= buf_uart_rx + c_UART_RXBUFLEN) {
@@ -71,7 +173,47 @@ ISR(USART_RX_vect) {
 	}
 }
 
+unsigned char f_uart_peek_char(void) {
+  unsigned char c;
+
+	if(v_uart_rxbuf_count > 0) {
+
+		c = *p_uart_rxout;
+		return c;
+	}
+  else {
+	  return 0xFF;
+	}
+}
+
+size_t f_uart_peek_str(unsigned char* str, unsigned int n) {
+  uint8_t i;
+	size_t len = 0;
+	unsigned char* p_str = str;
+	
+	if (v_uart_rxbuf_count == 0) {
+		return len;
+	}
+	
+	n--;
+	
+	if ( n > v_uart_rxbuf_count ) {
+		n = v_uart_rxbuf_count;
+	}
+	
+	for (i = 0; i < n; i++) {
+    *p_str++ = *(p_uart_rxout + i);
+	  len++;
+	}  
+
+	*p_str = '\0';
+	len++;
+		
+	return len;
+}
+
 unsigned char f_uart_get_char(void) {
+
 	unsigned char c;
 
 	if(v_uart_rxbuf_count > 0) {
@@ -90,35 +232,93 @@ unsigned char f_uart_get_char(void) {
 		return c;
 	}
   else {
-	  return -1;
+	  return 0xFF;
 	}
+}	
+
+unsigned char f_uart_get_char_ext(void) {
+	if (v_sys_state & bm_DBGEN) {
+		return 0xFF;
+	}
+	else {
+	  return f_uart_get_char();
+	}	
 }
 
-unsigned char f_uart_put_char(unsigned char c)
-{
-    if (v_uart_txbuf_count < c_UART_TXBUFLEN) {
-        cli();
-
-	++v_uart_txbuf_count;
-
-	*p_uart_txin = c;
-
-	if (++p_uart_txin >= buf_uart_tx + c_UART_TXBUFLEN) {
-            	p_uart_txin = buf_uart_tx;
+size_t f_uart_get_str(unsigned char* str, unsigned int n) {
+	
+	uint8_t i;
+	size_t len = 0;
+	unsigned char c;
+	unsigned char* p_str = str;
+	
+	n--;
+	
+	for (i = 0; i < n; i++) {
+		
+		c = f_uart_get_char();
+		
+		if (c != 0xFF) {
+			*p_str++ = c;
+			len++;
+		}
 	}
-        
-       	UCSR0B |= (1<<UDRIE0);
-        
-        sei();
+	
+	*p_str = '\0';
+	len++;
 
-	return 1;
-    }
-    else {
-        return 0;
-    }
+	return len;
 }
+
+size_t f_uart_get_str_ext(unsigned char* str, unsigned int n) {
+	if (v_sys_state & bm_DBGEN) {
+		return 0;
+	}
+	else {
+	  return f_uart_get_str(str, n);
+	}	
+}
+
+/*
+ * TODO:  optimize this
+ */
+size_t f_uart_put_char(unsigned char c) {
+	
+  if (v_uart_txbuf_count < c_UART_TXBUFLEN) {
+    
+	  cli();
+
+    ++v_uart_txbuf_count;
+
+    *p_uart_txin = c;
+
+    if (++p_uart_txin >= buf_uart_tx + c_UART_TXBUFLEN) {
+      p_uart_txin = buf_uart_tx;
+	  }
+
+    UCSR0B |= (1<<UDRIE0);
+        
+    sei();
+
+	  return 1;
+  }
+  else {
+    return 0;
+  }
+}
+
+
+size_t f_uart_put_char_ext(unsigned char c) {
+	if (v_sys_state & bm_DBGEN) {
+		return 0;
+	}
+	else {
+    return f_uart_put_char(c);
+	}	
+}		
 
 size_t f_uart_put_str(char s[]) {
+	
 	char *c = s;
 	size_t len = 0;
 
@@ -132,9 +332,28 @@ size_t f_uart_put_str(char s[]) {
 	return len;
 }
 
+size_t f_uart_put_str_ext(char s[]) {
+	if (v_sys_state & bm_DBGEN) {
+		return 0;
+	}
+	else {
+		return f_uart_put_str(s);
+	}
+}
+
 unsigned char f_uart_new_line(void) {
 	
 	return f_uart_put_str(UART_NL);
+}
+
+unsigned char f_uart_new_line_ext(void) {
+	if (v_sys_state & bm_DBGEN) {
+		return 0xFF;
+	}
+	else {
+		return f_uart_new_line();
+	}
+	
 }
 
 unsigned char f_Uart_ClrScr(void) {
